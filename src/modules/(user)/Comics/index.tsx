03863ea5bb6d763874821813ab7c283e5929b3d3
Jackie <1121@generative.xyz>
2024-06-28 00:16:17 +0700

    refactor: style using classname to style using module

--- src/modules/(user)/Comics/index.tsx
+++ src/modules/(user)/Comics/index.tsx
@@ -14,7 +14,10 @@ import { BaseGetResponse, BaseResponse } from "@/types/response";
 import { numberFormatter } from "@/utils/formatter";
 import { fromObjetToQuery } from "@/utils/utils";
 
-const ignoreData = {
+import styles from "./styles.module.scss";
+
+const initData = {
+  title: "",
   sortBy: ORDER_BY_OPTIONS[0].value,
   type: FILTER_COMIC_TYPE_OPTIONS[0].value,
   from: 0,
@@ -23,17 +26,11 @@ const ignoreData = {
 
 const ComicsModule = () => {
   const [data, setData] = React.useState<BaseGetResponse<Comic[]> | null>(null);
-  const [filter, setFilter] = React.useState<FormComicFilter>({
-    title: "",
-    sortBy: ORDER_BY_OPTIONS[0].value,
-    type: FILTER_COMIC_TYPE_OPTIONS[0].value,
-    from: 0,
-    to: 0,
-  });
+  const [filter, setFilter] = React.useState<FormComicFilter>(initData);
   const [page, setPage] = React.useState(1);
 
   const onFinish: FormProps<FormComicFilter>["onFinish"] = async (values: FormComicFilter) => {
-    const query = `/comics?pageNumber=1&size=6&status=APPROVE&` + fromObjetToQuery(values, ignoreData);
+    const query = `/comics?pageNumber=1&size=6&status=PENDING&` + fromObjetToQuery(values, initData);
     const { data } = (await AXIOS_INSTANCE.get<BaseResponse<BaseGetResponse<Comic[]>>>(query)).data;
 
     setPage(1);
@@ -43,7 +40,7 @@ const ComicsModule = () => {
 
   const onLoadMore = async () => {
     const nextPage = page + 1;
-    const query = `/comics?pageNumber=${nextPage}&size=6&` + fromObjetToQuery(filter, ignoreData);
+    const query = `/comics?pageNumber=${nextPage}&size=6&` + fromObjetToQuery(filter, initData);
     const { data } = (await AXIOS_INSTANCE.get<BaseResponse<BaseGetResponse<Comic[]>>>(query)).data;
 
     setPage(nextPage);
@@ -58,13 +55,13 @@ const ComicsModule = () => {
   };
 
   React.useEffect(() => {
-    onFinish(filter);
+    onFinish(initData);
   }, []);
 
   return (
     <React.Fragment>
-      <Container className="mb-[2rem]">
-        <div className="col-span-12 row-span-1 sticky top-0">
+      <Container className={styles.container}>
+        <div className={styles.container__filter}>
           <Form
             onFinish={onFinish}
             layout="vertical"
@@ -72,97 +69,96 @@ const ComicsModule = () => {
               sortBy: ORDER_BY_OPTIONS[0].value,
               type: FILTER_COMIC_TYPE_OPTIONS[0].value,
             }}
+            className={styles.form}
           >
-            <div className="w-full grid grid-cols-12 gap-[2rem]">
-              <Form.Item<FormComicFilter>
-                label={
-                  <Typography className="span" fontSize="sm">
-                    Comic title
-                  </Typography>
-                }
-                name="title"
-                className="col-span-2"
-              >
-                <Input placeholder="Naruto, Bleach, ..." />
-              </Form.Item>
+            <Form.Item<FormComicFilter>
+              label={
+                <Typography className="span" fontSize="sm">
+                  Comic title
+                </Typography>
+              }
+              name="title"
+              className={styles.form__item}
+            >
+              <Input placeholder="Naruto, Bleach, ..." />
+            </Form.Item>
 
-              <Form.Item<FormComicFilter>
-                label={
-                  <Typography className="span" fontSize="sm">
-                    Sort by
-                  </Typography>
-                }
-                name="sortBy"
-                className="col-span-2"
-              >
-                <Select options={ORDER_BY_OPTIONS} />
-              </Form.Item>
+            <Form.Item<FormComicFilter>
+              label={
+                <Typography className="span" fontSize="sm">
+                  Sort by
+                </Typography>
+              }
+              name="sortBy"
+              className={styles.form__item}
+            >
+              <Select options={ORDER_BY_OPTIONS} />
+            </Form.Item>
 
-              <Form.Item<FormComicFilter>
-                label={
-                  <Typography className="span" fontSize="sm">
-                    Comic type
-                  </Typography>
-                }
-                name="type"
-                className="col-span-2"
-              >
-                <Select options={FILTER_COMIC_TYPE_OPTIONS} />
-              </Form.Item>
+            <Form.Item<FormComicFilter>
+              label={
+                <Typography className="span" fontSize="sm">
+                  Comic type
+                </Typography>
+              }
+              name="type"
+              className={styles.form__item}
+            >
+              <Select options={FILTER_COMIC_TYPE_OPTIONS} />
+            </Form.Item>
 
-              <Form.Item<FormComicFilter>
-                label={
-                  <Typography className="span" fontSize="sm">
-                    From
-                  </Typography>
-                }
-                name="from"
-                className="col-span-2"
-              >
-                <InputNumber min={0} addonAfter="VND" formatter={(value) => numberFormatter(value || 0)} style={{ width: "100%" }} />
-              </Form.Item>
-              <Form.Item<FormComicFilter>
-                label={
-                  <Typography className="span" fontSize="sm">
-                    To
-                  </Typography>
-                }
-                name="to"
-                className="col-span-2"
-              >
-                <InputNumber min={0} addonAfter="VND" formatter={(value) => numberFormatter(value || 0)} style={{ width: "100%" }} />
-              </Form.Item>
+            <Form.Item<FormComicFilter>
+              label={
+                <Typography className="span" fontSize="sm">
+                  From
+                </Typography>
+              }
+              name="from"
+              className={styles.form__item}
+            >
+              <InputNumber min={0} addonAfter="VND" formatter={(value) => numberFormatter(value || 0)} style={{ width: "100%" }} />
+            </Form.Item>
+            <Form.Item<FormComicFilter>
+              label={
+                <Typography className="span" fontSize="sm">
+                  To
+                </Typography>
+              }
+              name="to"
+              className={styles.form__item}
+            >
+              <InputNumber min={0} addonAfter="VND" formatter={(value) => numberFormatter(value || 0)} style={{ width: "100%" }} />
+            </Form.Item>
 
-              <div className="col-span-2 flex items-center">
-                <Button type="primary" className="block w-full bg-[var(--color-primary)]" htmlType="submit">
-                  Search
-                </Button>
-              </div>
+            <div className="col-span-2 flex items-center">
+              <Button type="primary" className="block w-full" htmlType="submit">
+                Search
+              </Button>
             </div>
           </Form>
         </div>
 
-        {!data?.empty ? (
-          data?.content.map((comic) => (
-            <div key={comic.id} className="col-span-2 h-[36rem]">
-              <CardComic {...comic} />
+        <div className={styles.container__data}>
+          {!data?.empty ? (
+            data?.content.map((comic) => (
+              <div key={comic.id} className={styles.container__data__item}>
+                <CardComic {...comic} />
+              </div>
+            ))
+          ) : (
+            <div className={styles.container__data__empty}>
+              <Empty />
             </div>
-          ))
-        ) : (
-          <div className="col-span-12 flex justify-center">
-            <Empty />
-          </div>
-        )}
+          )}
+        </div>
       </Container>
 
-      <Container className="mb-[2rem]">
-        <div className="col-start-6 col-span-2 flex justify-center">
-          {data?.totalPages && page < data.totalPages ? (
-            <Button type="primary" className="bg-[var(--color-primary)]" onClick={onLoadMore}>
-              Load more
-            </Button>
-          ) : null}
-        </div>
+      <Container className={styles.container__pagination}>
+        {data?.totalPages && page < data.totalPages ? (
+          <Button type="primary" className="bg-[var(--color-primary)]" onClick={onLoadMore}>
+            Load more
+          </Button>
+        ) : null}
       </Container>
     </React.Fragment>
   );
